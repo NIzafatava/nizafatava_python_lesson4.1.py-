40e6e7906b299ce92a3be149362440902705f29c
Izafatava Nastassia <nizafatava@exadel.net>
2023-02-22 15:21:21 +0300

    lesson 1

---
+++ nizafatava_python_lesson1.py
@@ -0,0 +1,20 @@
+# задание №1
+a = 4
+b = 6
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a ** b)
+print(a / abs(b) )
+print(a // b)
+
+# задание №2
+
+x = (((17 / 2) * 3) + 2)
+print(x)
+
+x = (2 + ((17 / 2) * 3))
+print(x)
+
+x = 19 % 4 + 15
